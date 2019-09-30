8dfac11c47b5de279ecb17c4a89b6cf564ccb12d
Daniel Roe <daniel@concision.co.uk>
2019-09-30 16:40:08 +0200

    fix: prepare cache against workCache (#136)
    * fix: prepare cache against workCache
    
    Closes #133

--- src/prepare-cache.ts
+++ src/prepare-cache.ts
@@ -6,29 +6,18 @@ import consola from 'consola'
 import { startStep, endStep } from './utils'
 
 // Amend below line once https://github.com/zeit/now/issues/2992 is resolved
-async function prepareCache ({ cachePath, workPath, entrypoint }: PrepareCacheOptions): Promise<Record<string, FileRef>> {
+async function prepareCache ({ workPath, entrypoint }: PrepareCacheOptions): Promise<Record<string, FileRef>> {
   const entryDir = path.dirname(entrypoint)
-  const rootDir = path.join(workPath, entryDir)
-  const cacheDir = path.join(cachePath, entryDir)
-
-  consola.log('Cache dir:', cacheDir)
-
-  startStep('Clean cache')
-  await fs.remove(cacheDir)
-  await fs.mkdirp(cacheDir)
-  endStep()
 
   startStep('Collect cache')
   const cache: Record<string, FileRef> = {}
   for (const dir of ['.now_cache', 'node_modules_dev', 'node_modules_prod']) {
-    const src = path.join(rootDir, dir)
-    const dst = path.join(cacheDir, dir)
-    if (!fs.existsSync(src)) {
-      consola.warn(src, 'not exists. skipping!')
+    const activeDirectory = path.join(workPath, entryDir, dir)
+    if (!fs.existsSync(activeDirectory)) {
+      consola.warn(activeDirectory, 'not exists. skipping!')
       continue
     }
-    await fs.rename(src, dst)
-    const files = await glob(path.join(dir, '**'), cacheDir)
+    const files = await glob(path.join(entryDir, dir, '**'), workPath)
     consola.info(`${Object.keys(files).length} files collected from ${dir}`)
     Object.assign(cache, files)
   }
