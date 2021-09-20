cde59be7e80943fd1deb07d27afb898ac108f405
Steven <steven@ceriously.com>
2021-09-20 09:11:41 -0400

    Add `NUXT_CACHE_DISABLED` flag

--- src/prepare-cache.ts
+++ src/prepare-cache.ts
@@ -6,8 +6,12 @@ import { startStep, endStep } from './utils'
 async function prepareCache ({ workPath }: PrepareCacheOptions): Promise<Files> {
   startStep('Collect cache')
 
+  const dirs = process.env.NUXT_CACHE_DISABLED === '1'
+    ? []
+    : ['.nuxt', '.vercel_cache', 'node_modules_dev', 'node_modules_prod']
+
   const cache: Files = {}
-  for (const dir of ['.nuxt', '.vercel_cache', 'node_modules_dev', 'node_modules_prod']) {
+  for (const dir of dirs) {
     const files = await glob(`**/${dir}/**`, workPath)
     consola.info(`${Object.keys(files).length} files collected from ${dir}`)
     Object.assign(cache, files)
